fdde2605a5eb293bbf3475267970cf0addcae225
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-02-03 17:38:06 -0500

    removed some duplication in tests

--- spec/block_fall_spec.js
+++ spec/block_fall_spec.js
@@ -9,32 +9,33 @@ describe("BlockFall", function() {
 	expect($('#drawCanvas').length).toEqual(1);
     });
 
-    it("can draw the background", function() {
-	sinon.spy(blockFall.context, "fillRect");
-	blockFall.drawBackground();
-	expect(blockFall.context.fillRect.calledOnce).toBeTruthy();
-	sinon.spy(blockFall.context.fillRect.restore());
-    });
+    describe("drawing operations", function() {
+	beforeEach(function() {
+	    sinon.spy(blockFall.context, "fillRect");
+	});
 
-    it("draw a block", function() {
-	sinon.spy(blockFall.context, "fillRect");
-	blockFall.drawBlock(new Grid(1, 1), "red");
-	expect(blockFall.context.fillRect.calledOnce).toBeTruthy();
-	sinon.spy(blockFall.context.fillRect.restore());
-    });
+	afterEach(function() {
+	    sinon.spy(blockFall.context.fillRect.restore());
+	});
 
-    it("draws the border", function() {
-	sinon.spy(blockFall.context, "fillRect");
-	blockFall.drawBorder();
-	expect(blockFall.context.fillRect.callCount).toEqual(80);
-	sinon.spy(blockFall.context.fillRect.restore());
-    });
+	it("can draw the background", function() {
+	    blockFall.drawBackground();
+	    expect(blockFall.context.fillRect.calledOnce).toBeTruthy();
+	});
 
-    it("draw a square", function() {
-	sinon.spy(blockFall.context, "fillRect");
-	blockFall.drawSquare(2, 2);
-	expect(blockFall.context.fillRect.callCount).toEqual(4);
-	sinon.spy(blockFall.context.fillRect.restore());
-    });
+	it("draw a block", function() {
+	    blockFall.drawBlock(new Grid(1, 1), "red");
+	    expect(blockFall.context.fillRect.calledOnce).toBeTruthy();
+	});
 
+	it("draws the border", function() {
+	    blockFall.drawBorder();
+	    expect(blockFall.context.fillRect.callCount).toEqual(80);
+	});
+
+	it("draw a square", function() {
+	    blockFall.drawSquare(2, 2);
+	    expect(blockFall.context.fillRect.callCount).toEqual(4);
+	});
+    });
 });
