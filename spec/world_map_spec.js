5b20acf73c0db66b6684a61b16a587bf77389931
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-03-05 22:17:33 -0500

    added the ability to walk in 4 directions

--- spec/world_map_spec.js
+++ spec/world_map_spec.js
@@ -4,7 +4,7 @@ describe("WorldMap", function() {
 
     beforeEach(function() {
         gameInit = new GameInit(true);
-        worldMap = new WorldMap(window.Images);
+        worldMap = new WorldMap(window.Images, new Control());
         sinon.spy(worldMap.context, "drawImage");
     });
 
@@ -17,5 +17,41 @@ describe("WorldMap", function() {
         worldMap.draw();
         expect(worldMap.context.drawImage.callCount).toEqual(301);
     });
+
+
+    it("should let you move left", sinon.test(function() {
+        var worldMap = stubControl.call(this, 'isMovingLeft');
+        this.stub(worldMap, 'cecilWalkLeftFrame');
+        worldMap.update();
+        expect(worldMap.cecilWalkLeftFrame.callCount).toEqual(1);
+
+    }));
+
+    it("should let you move up", sinon.test(function() {
+        var worldMap = stubControl.call(this, 'isHardDropping');
+        this.stub(worldMap, 'cecilWalkUpFrame');
+        worldMap.update();
+        expect(worldMap.cecilWalkUpFrame.callCount).toEqual(1);
+    }));
+
+    it("should let you move right", sinon.test(function() {
+        var worldMap = stubControl.call(this, 'isMovingRight');
+        this.stub(worldMap, 'cecilWalkRightFrame');
+        worldMap.update();
+        expect(worldMap.cecilWalkRightFrame.callCount).toEqual(1);
+    }));
+
+    it("should let you move down", sinon.test(function() {
+        var worldMap = stubControl.call(this, 'isSoftDropping');
+        this.stub(worldMap, 'cecilWalkDownFrame');
+        worldMap.update();
+        expect(worldMap.cecilWalkDownFrame.callCount).toEqual(1);
+    }));
+
+    function stubControl(directionMethod) {
+        var control = new Control();
+        var stub = this.stub(control, directionMethod).returns(true);
+        return new WorldMap(window.Images, control);
+    };
 
 });
--- world_map.js
+++ world_map.js
@@ -1,7 +1,14 @@
 var WorldMap = Class.extend({
-    init: function(images) {
+    LEFT: 1,
+    UP: 2,
+    RIGHT: 3,
+    DOWN: 4,
+
+    init: function(images, control) {
         this.context = $('#gameCanvas').get(0).getContext("2d");
         this.images = images;
+        this.control = control;
+        this.direction = this.UP;
     },
 
     draw: function() {
@@ -9,14 +16,63 @@ var WorldMap = Class.extend({
             for (var j = 0; j < 15; j++)
                 this.context.drawImage(this.images.grass, i * 60, j * 60);
 
-        this.cecilWalkUpFrame();
+        if (this.direction == this.UP)
+            this.cecilWalkUpFrame();
+        else if (this.direction == this.LEFT)
+            this.cecilWalkLeftFrame();
+        else if (this.direction == this.RIGHT)
+            this.cecilWalkRightFrame();
+        else if (this.direction == this.DOWN)
+            this.cecilWalkDownFrame();
+    },
+
+    update: function() {
+        this.respondToControls();
+        this.draw();
+    },
+
+    respondToControls: function(input, arena) {
+        if (this.control.isMovingRight())
+            this.moveRight();
+        if (this.control.isMovingLeft())
+            this.moveLeft();
+        if (this.control.isHardDropping())
+            this.moveUp();
+        if (this.control.isSoftDropping())
+            this.moveDown();
     },
 
     cecilWalkUpFrame: function() {
-        this.context.drawImage.apply(this.context, [this.images.cecil, 0, 0, 60, 60].concat(this.centerOfScreen()).concat(this.scaleToSize()));
+        this.context.drawImage.apply(this.context, [this.images.cecil, 0, 0, 60, 60].concat(this.centerOfScreen()).concat([60, 60]));
     },
 
-    
+    cecilWalkRightFrame: function() {
+        this.context.drawImage.apply(this.context, [this.images.cecil, 395, 0, 45, 60].concat(this.centerOfScreen()).concat([45, 60]));
+    },
+
+    cecilWalkDownFrame: function() {
+        this.context.drawImage.apply(this.context, [this.images.cecil, 230, 0, 45, 60].concat(this.centerOfScreen()).concat([45, 60]));
+    },
+
+    moveRight: function() {
+        this.direction = this.RIGHT;
+    },
+
+    moveLeft: function() {
+        this.direction = this.LEFT;
+    },
+
+    moveUp: function() {
+        this.direction = this.UP;
+    },
+
+    moveDown: function() {
+        this.direction = this.DOWN;
+    },
+
+    cecilWalkLeftFrame: function() {
+        this.context.drawImage.apply(this.context, [this.images.cecil, 120, 0, 45, 60].concat(this.centerOfScreen()).concat([45, 60]));
+    },
 
     centerOfScreen: function() {
         return [9 * 60, 7 * 60];
