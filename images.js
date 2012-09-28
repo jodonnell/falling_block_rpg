5bd772d6a3daa7044335d3c6013f2605bb10de87
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-09-28 12:17:01 -0400

    removed unused images

--- images.js
+++ images.js
@@ -1,13 +1,5 @@
 var Images = Class.extend({
     init: function(callback) {
-        this.loadedImages = 0;
-        this.grass = new Image();
-        this.grass.src = "assets/grassTile.jpg";
-        //this.grass.onload = callback;
-
-        this.cecil = new Image();
-        this.cecil.src = "assets/cecil.png";
-        this.cecil.onload = callback;
 
     },
 
