0e7d7a5720035700fddbad08eca16fe94a82c452
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-09-28 11:53:06 -0400

    removed extra non tetris code

--- combatant.js
+++ combatant.js
@@ -1,24 +1,5 @@
 var Combatant = Class.extend({
     init: function(hp) {
         this.hp = hp;
-        this.max_hp = hp;
-        this.xp = 0;
-        this.level = 1;
-    },
-
-    addXp: function(xp) {
-        this.xp += xp;
-        if (this.didGainLevel())
-            this.gainLevel();
-    },
-
-    didGainLevel: function() {
-        return this.xp >= 100;
-    },
-
-    gainLevel: function() {
-        this.level++;
-        this.max_hp += 10;
-        this.hp = this.max_hp;
     }
 });
--- spec/combatant_spec.js
+++ spec/combatant_spec.js
@@ -3,24 +3,4 @@ describe("Combatant", function() {
         var combatant = new Combatant(100);
         expect(combatant.hp).toEqual(100);
     });
-
-    it("should have xp", function() {
-        var combatant = new Combatant(100);
-        expect(combatant.xp).toEqual(0);
-    });
-
-    it("should be able to give xp", function() {
-        var combatant = new Combatant(100);
-        combatant.addXp(5);
-        expect(combatant.xp).toEqual(5);
-    });
-
-    it("should be able to gain levels", function() {
-        var combatant = new Combatant(100);
-        combatant.addXp(100);
-        expect(combatant.level).toEqual(2);
-        expect(combatant.max_hp).toEqual(110);
-        expect(combatant.hp).toEqual(110);
-    });
-
 });
