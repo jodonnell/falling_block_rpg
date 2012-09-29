1575abbde3f1f024f84772eda06c393f8934ac72
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-09-29 16:56:47 -0400

    swapped the controls

--- control1.js
+++ control1.js
@@ -1,6 +1,6 @@
 var Control1 = Control.extend({
-    LEFT_KEY: 37,
-    UP_KEY: 38,
-    RIGHT_KEY: 39,
-    DOWN_KEY: 40,
+    LEFT_KEY: 65,
+    UP_KEY: 87,
+    RIGHT_KEY: 68,
+    DOWN_KEY: 83,
 });
--- control2.js
+++ control2.js
@@ -1,10 +1,6 @@
 var Control2 = Control.extend({
-    LEFT_KEY: 65,
-    LEFT_KEY_DVORAK: 65,
-    UP_KEY: 87,
-    UP_KEY_DVORAK: 188,
-    RIGHT_KEY: 68,
-    RIGHT_KEY_DVORAK: 69,
-    DOWN_KEY: 83,
-    DVORAK_DOWN_KEY: 79,
+    LEFT_KEY: 37,
+    UP_KEY: 38,
+    RIGHT_KEY: 39,
+    DOWN_KEY: 40,
 });
